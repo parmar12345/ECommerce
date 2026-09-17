063c3455bf20938a1b7317857d39756ce9bce899
01-folder <umang.parnar.it20@gmail.comemail@gmail.com>
2026-09-17 09:37:05 +0530

    add new  property in users

--- ECommerce.Domain/Entities/User.cs
+++ ECommerce.Domain/Entities/User.cs
@@ -16,6 +16,8 @@ public class User
 
     public DateTime? UpdatedAt { get; set; }
 
+    public int age { get; set; }
+
     public ICollection<PasswordResetToken> PasswordResetTokens
     { get; set; } = new List<PasswordResetToken>();
 
